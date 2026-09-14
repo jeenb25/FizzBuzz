0533b2e3a2c66d0d5dd8f202fd2019d34b90ba6a
jeenb25 <jeenb25@gmail.com>
2026-09-14 14:47:20 -0400

    helper function is now public!!!! yayyyyy

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -13,7 +13,7 @@ class FizzBuzz {
         }
     }
 
-    private static int helper(int i) {
+    public static int helper(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
